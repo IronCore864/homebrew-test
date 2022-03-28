e3cae44a72361ed6f6203476ea68b30db15868cd
Tiexin Guo <guotiexin@gmail.com>
2022-03-28 08:55:51 +0800

    Update devstream.rb

--- Formula/devstream.rb
+++ Formula/devstream.rb
@@ -1,4 +1,4 @@
-class DevStream < Formula
+class Devstream < Formula
     desc "DevStream: the open-source DevOps toolchain manager (DTM)."
     homepage "https://dtm.dev"
     url "https://github.com/merico-dev/stream/archive/refs/tags/v0.3.0.tar.gz"
